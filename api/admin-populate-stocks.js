961d8c1da01be10fa1b9140d55374ef8d088159b
MEKKTSX <kittisakworkshop@gmail.com>
2026-05-22 17:55:01 +0700

    Update admin-populate-stocks.js

--- api/admin-populate-stocks.js
+++ api/admin-populate-stocks.js
@@ -76,7 +76,7 @@ export default async function handler(req, res) {
                     queue.push(t.symbol);
                 }
             }
-            if (queue.length >= 1) break; 
+            if (queue.length >= 3) break; 
         }
 
         if (queue.length === 0) {
